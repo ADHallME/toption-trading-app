7b6c956f61b2d87560b2ce04e3ec38d1d493d25b
ADHallME <halla@virteraenergy.com>
2025-09-20 10:16:46 -0400

    Temporarily disable middleware to fix 500 error

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from '@clerk/nextjs'
-
-export default authMiddleware({
-  publicRoutes: ['/', '/sign-in', '/sign-up', '/api/webhook']
-})
-
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
